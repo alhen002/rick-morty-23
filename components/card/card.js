41ace394230cb84ebc71faeb90dc1cc45baabaac
Alexander Henting <a.henting@posteo.de>
2023-09-06 15:56:48 +0200

    added the fetch function and rendering the different cards

--- components/card/card.js
+++ components/card/card.js
@@ -11,7 +11,7 @@ export function createCharacterCard({ image, name, status, type, episode }) {
     <div class="card__image-gradient"></div>
     </div>
   <div class="card__content">
-    <h2 class="card__title>${name}</h2>
+    <h2 class="card__title">${name}</h2>
     <dl class="card__info">
       <dt class="card__info-title">Status</dt>
       <dd class="card__info-description">${status}</dd>
--- index.js
+++ index.js
@@ -8,7 +8,37 @@ const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
+// components
+import { createCharacterCard } from "./components/card/card.js";
+
 // States
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+const BASEURL = "https://rickandmortyapi.com/api/character";
+
+async function fetchCharacters() {
+  cardContainer.innerHTML = "";
+  const response = await fetch(BASEURL + `?page=${page}`);
+
+  if (!response.status === "200") {
+    throw new Error("There is an error in the fetch");
+  }
+
+  try {
+    const json = await response.json();
+    const chars = json.results;
+    return chars;
+  } catch (error) {
+    console.log(`${error.status} - ${error.message}`);
+  }
+}
+
+const characters = await fetchCharacters();
+console.log(characters);
+
+characters.forEach((character) => {
+  const card = createCharacterCard(character);
+  cardContainer.appendChild(card);
+});
